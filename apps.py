7d489f187e15f7c79bc02a735d2681acc7233310
Shrinivas Vasant Shanbhag <shrinivasvshanbhag98@gmail.com>
2019-04-08 20:17:09 +0530

    updated views,urls,forms

---
+++ apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+for i in range(2):
+	server_in_123=i
+
+class LandingConfig(AppConfig):
+    name = 'landing'
---
+++ views.py
@@ -0,0 +1,79 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.contrib.auth import logout as django_logout
+	
+# Create your views here.
+
+def home(request):
+    """
+    Home function
+    """
+    print("Home")
+    return render(request, 'landing/index.html', {'user':request.user})
+
+
+
+
+def login1(request):
+    """
+    Return login page
+    """
+
+    return render(request, 'landing/login.html')
+
+def logout(request):
+    """
+    Return logout page
+    """
+    django_logout(request)
+    print("Logging out")
+    return redirect('/')
+
+
+def signup(request):
+    """
+    Return sign up page
+    """
+    return render(request, 'landing/signup.html')
+
+
+def signup_submit(request):
+    """
+    function which runs after clicking sign up submit
+    """
+    print("Creating a new user")
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    email = request.POST.get('email')
+    if username == '' or password == '' or email == "":
+        return render(request, 'landing/signup.html')
+
+    user = User.objects.create_user(username=username, email=email, password=password)
+    user.save()
+
+    return redirect('/')
+
+
+
+def logging_in(request):
+    """
+    Function which runs after log in button is pressed
+
+    """
+    username = request.POST['username']
+    password = request.POST['password']
+    if username == '' or password == '':
+        return render(request, 'landing/login.html')
+    user = authenticate(request, username=username, password=password)
+    print(user)
+    if user is not None:
+        login(request,user)
+        return redirect('/', {'user':request.user})
+        # Redirect to a success page.
+        ...
+    else:
+        return redirect('/login')
+        # Return an 'invalid login' error message.
+        ...	
